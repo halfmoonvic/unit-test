78b6a9b87f3d5835a802156a32c5bcc460cca743
cs-mac <halfmoonvic@gmail.com>
2019-10-12 18:25:24 +0800

    feat: jest mocha 等库的运行原理

---
+++ test/testdemo.js
@@ -0,0 +1,23 @@
+const { add } = require('../src/math');
+
+function test(description, fn) {
+  try {
+    fn();
+  } catch (e) {
+    console.error(`failed: ${description}. at ${e}`);
+  }
+}
+
+function expect(received) {
+  return {
+    toBe: expected => {
+      if (received !== expected) {
+        throw new Error(`expected ${expected} and received ${received}`);
+      }
+    }
+  };
+}
+
+test('should return 5 when 2 + 3', () => {
+  expect(add(2, 3)).toBe(5);
+});
