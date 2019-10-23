777780984d4d8c8aa035911f0568e44cc20230fd
cs-mac <halfmoonvic@gmail.com>
2019-10-23 10:25:30 +0800

    chore: eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,19 +3,38 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ["plugin:vue/essential", "@vue/prettier", "@vue/typescript"],
+  extends: ['plugin:vue/essential', '@vue/prettier', '@vue/typescript'],
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    'no-console': ['warn', { allow: ['debug', 'info', 'warn', 'error', 'clear'] }],
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'comma-dangle': [1, 'never'],
+    eqeqeq: 1,
+    indent: [1, 2],
+    'generator-star-spacing': 0,
+    'no-console': 0,
+    'no-debugger': process.env.NODE_ENV === 'production' ? 0 : 1,
+    'no-multiple-empty-lines': [1, { max: 2 }],
+    'no-multi-spaces': 1,
+    'no-unused-vars': 1,
+    semi: [1, 'always'],
+    'space-before-function-paren': [
+      1,
+      {
+        anonymous: 'never',
+        named: 'never',
+        asyncArrow: 'always'
+      }
+    ],
+    'spaced-comment': 1
   },
   parserOptions: {
-    parser: "@typescript-eslint/parser"
+    parser: '@typescript-eslint/parser'
   },
   overrides: [
     {
       files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)"
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)'
       ],
       env: {
         jest: true
