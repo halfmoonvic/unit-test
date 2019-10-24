d00441fb1f6b818c9faff13ee1989577559fb8da
cs-mac <halfmoonvic@gmail.com>
2019-10-24 11:11:56 +0800

    feat: 简单的测试

--- tests/unit/HelloWorld.spec.ts
+++ tests/unit/HelloWorld.spec.ts
@@ -1,12 +1,29 @@
 import { shallowMount } from '@vue/test-utils';
 import HelloWorld from '@/components/HelloWorld.vue';
+import Vue from 'vue';
 
 describe('HelloWorld.vue', () => {
+  it('通过手写 html 等来测试', () => {
+    const root = document.createElement('div');
+    root.className = 'root';
+    document.body.appendChild(root);
+    new Vue({
+      render: h =>
+        h(HelloWorld, {
+          props: {
+            msg: 'msg'
+          }
+        })
+    }).$mount('.root');
+    expect(document.getElementsByClassName('hello').length).toBe(1);
+  });
+
   it('renders props.msg when passed', () => {
-    const msg = 'new message';
+    const msg = 'new message abc';
     const wrapper = shallowMount(HelloWorld, {
       propsData: { msg }
     });
     expect(wrapper.text()).toMatch(msg);
+    expect(wrapper).toMatchSnapshot();
   });
 });
